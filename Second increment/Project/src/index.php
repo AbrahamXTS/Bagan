4c7768fac70e7592d1b941a1d73cc026692dee9d
Abraham Espinosa <abrahamespinosa3132@gmail.com>
2022-04-14 17:58:57 -0500

    Template del proyecto

---
+++ Second increment/Project/src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link href="../dist/output.css" rel="stylesheet" />
+</head>
+
+<body>
+	<div class="container">
+		<img src="img/logo.png" alt="Logo" class="mx-auto w-56 h-56">
+		<h1 class="text-rojo">Inicio de sesión</h1>
+		<h2>Bienvenido de vuelta</h2>
+	</div>
+</body>
+
+</html>
--- Second increment/Project/tailwind.config.js
+++ Second increment/Project/tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,php}"],
   theme: {
+    colors: {
+      "rojo": "#D43031",
+      "gris": "#D1D5DB",
+      "texto": "#AAAAAA",
+    },
     extend: {},
   },
   plugins: [],
