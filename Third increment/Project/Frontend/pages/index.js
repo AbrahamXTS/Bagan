8404936871d3cf9a65a9174c6f2f1d7e9e575715
Abraham Espinosa <abrahamespinosa3132@gmail.com>
2022-05-05 02:00:49 -0500

    Scaffold Third increment

---
+++ Third increment/Project/Frontend/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { useState } from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Login</title>
+        <meta name="description" content="Gestión de almacén CECATI" />
+      </Head>
+    </div>
+  )
+}
